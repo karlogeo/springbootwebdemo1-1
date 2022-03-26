98bf2466aa8607793f1dd651ae2d3aedb3700211
karlogeo <karlogeo@gmail.com>
2022-03-26 14:32:45 +0800

    init

---
+++ src/main/java/com/example/demo/Springbootwebdemo11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootwebdemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootwebdemo11Application.class, args);
+	}
+
+}
